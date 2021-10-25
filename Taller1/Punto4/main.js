fc4723059a1da52c1553859eb97d31a919139b4f
Miguel Angel <angelpalacio2772@gmail.com>
2021-10-25 16:45:56 -0500

    punto 4 resuelto

---
+++ Taller1/Punto4/main.js
@@ -0,0 +1,9 @@
+var vo=0
+var a=8
+var t=5
+
+function velocidad (){
+ var veloit = vo + a * t
+ var velomd = (vo + veloit)/2
+ alert ("velocidad inicial es "+veloit+" velocidad media " + velomd)
+}
