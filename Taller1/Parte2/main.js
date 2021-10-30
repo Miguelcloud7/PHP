c7da68dcd6cb56fe960990e8283243b34f9c0c53
Mariafmm <mariasillita28@gmai.com>
2021-10-30 18:49:12 -0500

    version estable

--- Taller1/Parte2/main.js
+++ Taller1/Parte2/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-    document.getElementById("jq").innerHTML = "Version de jQuery 3.5.1"; 
-  });
+    document.getElementById("jq").innerHTML = "Version de jQuery 3.5.1";
+});
+$("#resultado").hide();
 
+function login() {
+
+    $("#resultado").show("3000");
+    event.preventDefault();
+
+    var nombre = document.getElementById("nombre").value;
+    var tipoid = document.getElementById("tipoid").value;
+    var email = document.getElementById("email").value;
+    var tel = document.getElementById("tel").value;
+    var direccion = document.getElementById("direccion").value;
+    var pass = document.getElementById("pass").value;
+    var cpass = document.getElementById("cpass").value;
+    var doc = document.getElementById("documento").value;
+
+    alert(doc)
+}
