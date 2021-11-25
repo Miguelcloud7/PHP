d5bac92d7a05f72de663f2ea09008d4e6693779e
Dsr2004 <davitdy2015@gmail.com>
2021-11-25 08:46:59 -0500

    Punto A sin validaciones

--- FundamentosPHP/PuntoA/funcion.php
+++ FundamentosPHP/PuntoA/funcion.php
@@ -3,12 +3,11 @@ $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $num3 = $_POST['num3'];
 
-// function sumar($num1,$num2,$num3){
-//     $suma=$num1+$num2+$num3;
-//     return ("la suma es ".$suma);
-// };
-// $r=sumar($num1,$num2,$num3);
-// echo ($r);
+function sumar($num1,$num2,$num3){
+    $suma=$num1+$num2+$num3;
+    echo ("la suma es ".$suma);
+};
+sumar($num1,$num2,$num3);
 
-echo ($num1+$num2+$num3)
+
 ?>
--- FundamentosPHP/Puntoc/controlador.php
+++ FundamentosPHP/Puntoc/controlador.php
@@ -5,24 +5,25 @@
     $ef=$_POST["ef"];
     $tf=$_POST["tf"];
 
-    // $errores=[];
+    $errores=[];
+    $resultado=[]
 
-    // if (!is_numeric($n1)){
-    //     $errores["n1"]=True;
-    // }
-    // if (!is_numeric($n2)){
-    //     $errores["n2"]=True;
-    // }
-    // if (!is_numeric($n3)){
-    //     $errores["n3"]=True;
-    // }
-    // if (!is_numeric($ef)){
-    //     $errores["ef"]=True;
-    // }
-    // if (!is_numeric($tf)){
-    //     $errores["tf"]=True;
-    // }
-
+    if (!is_numeric($n1)){
+        $errores["n1"]=True;
+    }
+    if (!is_numeric($n2)){
+        $errores["n2"]=True;
+    }
+    if (!is_numeric($n3)){
+        $errores["n3"]=True;
+    }
+    if (!is_numeric($ef)){
+        $errores["ef"]=True;
+    }
+    if (!is_numeric($tf)){
+        $errores["tf"]=True;
+    }
+    $notasf=($n1+$n2+$n3)
 
     echo($n1." ".$n2." ".$n3." ".$ef." ".$tf)
 
