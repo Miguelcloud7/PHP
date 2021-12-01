76ac9bb8c051a5e659d53f413e60b672d3c10fab
Hoodinii890 <jhormangallegogallego@gmail.com>
2021-12-01 16:02:05 -0500

    Actualización de los números romanos

--- Taller1/Punto3/main.js
+++ Taller1/Punto3/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-  document.getElementById("jq").innerHTML = "Version de jQuery 3.5.1"; 
+    document.getElementById("jq").innerHTML = "Version de jQuery 3.5.1";
 });
-function convertToRoman() {
-  let n = document.getElementById("num").value;
-  var
-  valores = [1, 5, 10, 50, 100, 500, 1000],
-  letras = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
-  res = [],
-  num, letra
 
-for(var i = 6; num = valores[i], letra = letras[i]; i--) {
-  if(n >= num) {
-    var r = Math.floor(n / num); 
-    n -= r * num; 
-    if(r < 4){
-      while(r--){
-        res.push(letra);
-      }
+function convertToRoman() {
+    let n = document.getElementById("num").value;
+    var
+        valores = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000],
+        letras = ['I', 'I,V', 'V', 'I,X', 'X', 'X,L', 'L', 'X,C', 'C', 'C,D', 'D', 'C,M', 'M'],
+        res = [],
+        num, letra
+    for (var i = valores.length - 1; num = valores[i], letra = letras[i]; i--) {
+        if (n >= num) {
+            var r = Math.floor(n / num);
+            n -= r * num;
+            if (r <= 4) {
+                while (r--) {
+                    res.push(letra);
+                }
+            }
+        }
     }
-  }
-} 
-document.getElementById("romano").innerHTML = res
+    document.getElementById("romano").innerHTML = res
 }
