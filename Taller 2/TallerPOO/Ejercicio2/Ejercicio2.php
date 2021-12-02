20b8cb0d83f249dbfbc8fd8cf852495fe4f67a04
Hoodinii890 <jhormangallegogallego@gmail.com>
2021-12-01 20:54:15 -0500

    dashboard

---
+++ Taller 2/TallerPOO/Ejercicio2/Ejercicio2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
+<?php 
+class cuentaBancaria {
+    var $nombreCuenta;
+    var $docId;
+    var $saldoActual;
+    var $interes;
+}
+?>
